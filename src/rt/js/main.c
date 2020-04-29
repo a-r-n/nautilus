ffb61f930b5c29827187e1561c4e47ae350c05cd
Aaron Nelson <aaron@robertnelson.xyz>
2020-04-29 11:35:11 -0400

    Pass main errors, linker fails

---
+++ src/rt/js/main.c
@@ -0,0 +1,16 @@
+#include <nautilus/nautilus.h>
+#include <nautilus/shell.h>
+
+
+static int entry(char* buf, void* priv) {
+    printf("TEST\n");
+    return 0;
+}
+
+static struct shell_cmd_impl js_impl = {
+    .cmd    = "js",
+    .help_str = "js",
+    .handler = entry
+};
+
+nk_register_shell_cmd(js_impl);
